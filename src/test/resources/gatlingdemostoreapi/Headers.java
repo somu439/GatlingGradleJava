1f26c2f330585bc87bd23545e197e88a2aff5264
Sreeni <somu439@gmail.com>
2023-04-16 14:45:15 -0400

    Initial commit - basic performance test

--- src/test/resources/gatlingdemostoreapi/Headers.java
+++
@@ -1,9 +0,0 @@
-package gatlingdemostoreapi;
-
-import java.util.Map;
-
-public class Headers {
-    public static Map<CharSequence, String> authorizationHeaders = Map.ofEntries(
-            Map.entry("authorization", "Bearer #{jwt}")
-    );
-}
